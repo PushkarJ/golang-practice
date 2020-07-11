9459070ff98607075af21f8c511bc449d711f5f9
Pushkar Joglekar <pushkar.joglekar1789@gmail.com>
2020-07-11 10:54:20 -0700

    Added funcs for maxDepth of Binary Tree and detection Symmetric/Mirror trees

--- trees/algos.go
+++ trees/algos.go
@@ -1,3 +1,11 @@
+/*
+
+Notes:
+
+1. Pass arguments for struct vars as reference; its easier to check for nil references
+2. ALWAYS: Check node for nil-ness as well as left and/or right to prevent referencing like this (nil).left that causes panic
+
+*/
 package trees
 
 import (
@@ -12,28 +20,56 @@ type Node struct {
 
 func Algos() {
 	fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(nil))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(nil))
+	fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(nil))
 	root := Node{left: nil, right: nil, value: 1}
 	fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+	fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
 	left := Node{left: nil, right: nil, value: 2}
 	root = Node{left: &left, right: nil, value: 1}
 	fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+	fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
 	right := Node{left: nil, right: nil, value: 3}
 	root = Node{left: nil, right: &right, value: 1}
 	fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+	fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
 	root = Node{left: &left, right: &right, value: 1}
+	root.right.value = 2
 	fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+	fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
 	leftOfleft := Node{left: nil, right: nil, value: 4}
 	left = Node{left: &leftOfleft, right: nil, value: 2}
 	root = Node{left: &left, right: &right, value: 1}
 	fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+	fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
 	rightOfright := Node{left: nil, right: nil, value: 5}
 	right = Node{left: nil, right: &rightOfright, value: 3}
 	root = Node{left: &left, right: &right, value: 1}
 	fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+	fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
 	leftOfright := Node{left: nil, right: nil, value: 6}
 	right = Node{left: &leftOfright, right: &rightOfright, value: 3}
 	root = Node{left: &left, right: &right, value: 1}
 	fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+	fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
+	rightOfLeft := Node{left:nil, right: nil, value: 6}
+	left = Node{left:nil, right: &rightOfLeft, value: 2}
+	right = Node{left: &leftOfright, right: nil, value: 2}
+        fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+        fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
+        left = Node{left:nil, right: nil, value: 2}
+        right = Node{left: &leftOfright, right: nil, value: 2}
+        fmt.Println("Minimum depth of BTree: ", findMinDepthOfBTree(&root))
+        fmt.Println("Maximum depth of BTree: ", findMaxDepthOfBTree(&root))
+        fmt.Println("Is Tree symmetrical? ", isTreeSymmetrical(&root))
 }
 
 func findMinDepthOfBTree(root *Node) int {
@@ -61,6 +97,26 @@ func findMinDepthOfBTree(root *Node) int {
 	}
 }
 
+func findMaxDepthOfBTree(node *Node)(int){
+
+	if node == nil {
+                return 0
+        }
+        if isNodeLeaf(node) {
+                return 1
+        } else if isNodeLeaf(node.left) && isNodeLeaf(node.right) {
+                return 2
+        }else{
+		left := 1 + findMaxDepthOfBTree(node.left)
+		right := 1 + findMaxDepthOfBTree(node.right)
+		if left > right{
+			return left
+		}else{
+			return right
+		}
+	}
+}
+
 func isNodeLeaf(node *Node) bool {
 	if node == nil {
 		return false
@@ -72,10 +128,30 @@ func isNodeLeaf(node *Node) bool {
 	return false
 }
 
-/*
+func isTreeSymmetrical(root *Node)(bool) {
 
-Notes:
+	if root == nil || isNodeLeaf(root) {
+		return true
+	} else {
+		return areTwoTreesMirrorTrees(root.left, root.right)
+	}
 
-1. Pass arguments for struct vars as reference; its easier to check for nil references
-2. Check node for nil-ness as well as left and/or right to prevent referencing like this (nil).left that causes panic
-*/
+}
+
+func areTwoTreesMirrorTrees(left *Node, right *Node)(bool) {
+	if left == right && left == nil{
+		return true
+	}
+	if (left == nil && right != nil) || (right == nil && left != nil){
+		return false
+	}
+	if isNodeLeaf(left) && isNodeLeaf(right) {
+		return left.value == right.value
+	} else if isNodeLeaf(left) != isNodeLeaf(right) {
+		return false
+	}
+	//This means both are not leaf nodes
+
+	return left.value == right.value && areTwoTreesMirrorTrees(left.right, right.left) && areTwoTreesMirrorTrees(left.left, right.right)
+}
+
