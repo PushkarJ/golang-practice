76b508aeb7b980c0bf91ac0dac566c805f452551
Pushkar Joglekar <pushkar.joglekar1789@gmail.com>
2020-08-08 11:49:09 -0700

    Find m to power n

---
+++ numbers/algos.go
@@ -0,0 +1,35 @@
+import "math"
+
+//As tested on leetcode
+
+func myPow(x float64, n int) float64 {
+    answer := 0.0
+    if x == 0 || math.Abs(x) == 1{
+        if math.Mod(float64(n), 2) == 0{
+            return math.Abs(x)
+        }
+        return x
+    }
+
+    absoluteN := n
+    if n == 0 {
+        answer = 1
+    }else if n > 0{
+        answer = x
+        absoluteN = n
+    }else if n < 0 {
+        answer = 1/x
+        absoluteN = -n
+    }
+    
+    for i := 1; i < absoluteN; i++{
+        if n > 1{
+            answer = answer * x  
+        }
+        if n < 1 {
+            answer = answer * ( 1/x )
+        }
+    }
+    
+    return answer
+}
