04dbf2b2b23e7803bc6b6db1b5b1e44143dd6989
Pushkar Joglekar <pushkar.joglekar1789@gmail.com>
2020-09-06 11:58:29 -0700

    Merge two sorted list

--- ll/algos.go
+++ ll/algos.go
@@ -14,6 +14,8 @@ func Algos() {
 	l2 := &ListNode{Val: 2, Next: nil}
 	l3 := addTwoNumbers(l1, l2)
 	fmt.Println("Sum of number: ", l1.Next.Next.Val, l1.Next.Val, l1.Val, "and number: ", l2.Val, "is: ", l3.Next.Next.Next.Val, l3.Next.Next.Val, l3.Next.Val, l3.Val)
+	l3 = mergeTwoLists(l1, l2)
+	fmt.Println("Merged list of this: ", l1.Val, l1.Next.Val, l1.Next.Next.Val, "and this: ", l2.Val, " is: ", l3.Val, l3.Next.Val, l3.Next.Next.Val, l3.Next.Next.Next.Val)
 }
 
 /**
@@ -85,4 +87,57 @@ func addOneByOne(v1 int, v2 int, carryover int) (int, int) {
 		return 1, val
 	}
 	return 0, val
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	}
+	var l3 *ListNode
+	k := l3
+	if l1.Next == l2.Next && l1.Next == nil {
+		if l1.Val < l2.Val {
+			l1.Next = l2
+			return l1
+		} else {
+			l2.Next = l1
+			return l2
+		}
+	}
+	if l1.Val <= l2.Val {
+		k = l1
+		l3 = k
+		l1 = l1.Next
+		k.Next = nil
+	} else {
+		k = l2
+		l3 = k
+		l2 = l2.Next
+		k.Next = nil
+	}
+	for {
+
+		if l1 != nil && l2 != nil {
+			if l1.Val < l2.Val && k.Val <= l1.Val {
+				k.Next = l1
+				l1 = l1.Next
+			} else if l1.Val >= l2.Val && k.Val <= l2.Val {
+				k.Next = l2
+				l2 = l2.Next
+			}
+			k = k.Next
+		} else if l1 != nil && l2 == nil {
+			k.Next = l1
+			break
+		} else if l1 == nil && l2 != nil {
+			k.Next = l2
+			break
+		} else {
+			break
+		}
+	}
+	return l3
 }
